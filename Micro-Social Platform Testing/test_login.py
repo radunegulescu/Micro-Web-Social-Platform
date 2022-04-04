acb2a9c0635e77c141ee0313e54230b066e5a485
Radu Negulescu <radu.negulescu@s.unibuc.ro>
2022-04-04 23:02:42 +0300

    Login + Register tests

--- Micro-Social Platform Testing/test_login.py
+++ Micro-Social Platform Testing/test_login.py
@@ -1,4 +1,3 @@
-import pytest
 from testFramework import login
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
@@ -14,11 +13,12 @@ def setup_module():
 
 class Test_Login():
     def teardown_method(self):
-        if not driver.current_url.startswith(
-                'http://localhost:54805/Account/Login'):
-            logoff_button = WebDriverWait(driver, 10).until(
-                EC.visibility_of_element_located((By.XPATH, "//*[@id='logoutForm']/ul/li[2]/a")))
-            logoff_button.click()
+        # if not driver.current_url.startswith(
+        #         'http://localhost:54805/Account/Login'):
+        #     logoff_button = WebDriverWait(driver, 10).until(
+        #         EC.visibility_of_element_located((By.XPATH, "//*[@id='logoutForm']/ul/li[2]/a")))
+        #     logoff_button.click()
+        driver.get("http://localhost:54805/Account/Login")
 
     def test_login_succesfull(self):
         assert(login.enter_email(driver, "andreineagu672@gmail.com"))
@@ -27,7 +27,21 @@ class Test_Login():
         assert(driver.current_url is not None and driver.current_url ==
                "http://localhost:54805/")
 
-    def test_login_failed(self):
+    def test_login_failed_email(self):
+        assert(login.enter_email(driver, "andreineagu672"))
+        assert(login.enter_password(driver, "Parola1234!"))
+        assert(login.press_login_button(driver))
+        assert(driver.current_url is not None and driver.current_url.startswith(
+            'http://localhost:54805/Account/Login'))
+
+    def test_login_failed_email_empty(self):
+        assert(login.enter_email(driver, ""))
+        assert(login.enter_password(driver, "Parola1234!"))
+        assert(login.press_login_button(driver))
+        assert(driver.current_url is not None and driver.current_url.startswith(
+            'http://localhost:54805/Account/Login'))
+
+    def test_login_failed_password(self):
         assert(login.enter_email(driver, "andreineagu672@gmail.com"))
         assert(login.enter_password(driver, "Parola1234!"))
         assert(login.press_login_button(driver))
