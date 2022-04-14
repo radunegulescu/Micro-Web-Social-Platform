ad704136a44138e91e5da66f568f82cd0f68d31f
Oana Dima <61749814+DimaOanaTeodora@users.noreply.github.com>
2022-04-14 12:46:39 +0300

    Add files via upload

--- Micro-Social Platform Testing/test_view_friend_profile.py
+++ Micro-Social Platform Testing/test_view_friend_profile.py
@@ -8,13 +8,13 @@ driver = webdriver.Chrome()
 def setup_module():
     driver.get("http://localhost:54805/Account/Login")
 
-class Test_Add_Friend():
+class Test_View_Friend():
     def teardown_method(self):
         driver.get("http://localhost:54805/Account/Login")
 
     def test_login_successful(self):
-        assert(login.enter_email(driver, "marinela@gmail.com"))
-        assert(login.enter_password(driver, "*123Marinela"))
+        assert(login.enter_email(driver, "andreineagu672@gmail.com"))
+        assert(login.enter_password(driver, "Parola1234!"))
         assert(login.press_login_button(driver))
         assert(driver.current_url is not None and driver.current_url ==
                "http://localhost:54805/")
@@ -24,26 +24,30 @@ class Test_Add_Friend():
 
         driver.get("http://localhost:54805/Users")
         
-        user = "andreineagu672@gmail.com"
+        user = "Marinela"
         assert(view_friend_profile.press_only_friends_button(driver))
         assert(add_a_friend.enter_user_name(driver, user))
         assert(add_a_friend.press_search_user_button(driver))
         assert(view_friend_profile.press_view_pofile_button(driver))
         assert(view_friend_profile.test_profile_name(driver, user))
-        assert(driver.current_url is not None and driver.current_url ==
-               "http://localhost:54805/")
+        assert((driver.current_url is not None) 
+                        and 
+               ("http://localhost:54805/Users/Show/" in  driver.current_url)
+              )
 
     def test_view_friend_profile_failed(self):
         self.test_login_successful()
 
         driver.get("http://localhost:54805/Users")
         
-        user = "protocaliu"
+        user = "marinela@gmail.com" # searching by username, to email
         assert(view_friend_profile.press_only_friends_button(driver))
         assert(add_a_friend.enter_user_name(driver, user))
         assert(add_a_friend.press_search_user_button(driver))
         assert(view_friend_profile.press_view_pofile_button(driver))
         assert(view_friend_profile.test_profile_name(driver, user))
-        assert(driver.current_url is not None and driver.current_url ==
-               "http://localhost:54805/")
+        assert((driver.current_url is not None) 
+                        and 
+               ("http://localhost:54805/Users/Show/" in  driver.current_url)
+              )
     
