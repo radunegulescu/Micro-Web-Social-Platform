72b01e62727fdf9898deb1152d835c8c5f8e2d8d
Oana Dima <61749814+DimaOanaTeodora@users.noreply.github.com>
2022-04-14 13:16:31 +0300

    Correct bugs for view friend profile

--- Micro-Social Platform Testing/test_view_friend_profile.py
+++ Micro-Social Platform Testing/test_view_friend_profile.py
@@ -12,15 +12,13 @@ class Test_View_Friend():
     def teardown_method(self):
         driver.get("http://localhost:54805/Account/Login")
 
-    def test_login_successful(self):
+    def test_view_friend_profile_successful(self):
+        # login
         assert(login.enter_email(driver, "andreineagu672@gmail.com"))
-        assert(login.enter_password(driver, "Parola1234!"))
+        assert(login.enter_password(driver, "Parola123!"))
         assert(login.press_login_button(driver))
         assert(driver.current_url is not None and driver.current_url ==
                "http://localhost:54805/")
-
-    def test_view_friend_profile_successful(self):
-        self.test_login_successful()
 
         driver.get("http://localhost:54805/Users")
         
@@ -36,18 +34,24 @@ class Test_View_Friend():
               )
 
     def test_view_friend_profile_failed(self):
-        self.test_login_successful()
+        "Empty searchg scenario"
+        
+        #login
+        assert(login.enter_email(driver, "andreineagu672@gmail.com"))
+        assert(login.enter_password(driver, "Parola123!"))
+        assert(login.press_login_button(driver))
+        assert(driver.current_url is not None and driver.current_url ==
+               "http://localhost:54805/")
 
         driver.get("http://localhost:54805/Users")
         
-        user = "marinela@gmail.com" # searching by username, to email
+        user = "marinela@gmail.com" # searching by username, not by email
+        # empty search 
         assert(view_friend_profile.press_only_friends_button(driver))
         assert(add_a_friend.enter_user_name(driver, user))
         assert(add_a_friend.press_search_user_button(driver))
-        assert(view_friend_profile.press_view_pofile_button(driver))
-        assert(view_friend_profile.test_profile_name(driver, user))
         assert((driver.current_url is not None) 
                         and 
-               ("http://localhost:54805/Users/Show/" in  driver.current_url)
+               ("http://localhost:54805/Users?search=" in  driver.current_url)
               )
     
