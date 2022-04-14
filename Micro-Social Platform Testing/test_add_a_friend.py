ab4e4d45fcac8e7cd74717242ac84aa0e70b3fa5
Oana Dima <61749814+DimaOanaTeodora@users.noreply.github.com>
2022-04-14 13:27:30 +0300

    Remove bugs from add a friend

--- Micro-Social Platform Testing/test_add_a_friend.py
+++ Micro-Social Platform Testing/test_add_a_friend.py
@@ -17,15 +17,14 @@ class Test_Add_Friend():
     def teardown_method(self):
         driver.get("http://localhost:54805/Account/Login")
 
-    def test_login_successful(self):
-        assert(login.enter_email(driver, "marinela@gmail.com"))
-        assert(login.enter_password(driver, "*123Marinela"))
+    def test_add_friend_successful(self):
+        # login
+
+        assert(login.enter_email(driver, "andreineagu672@gmail.com"))
+        assert(login.enter_password(driver, "Parola123!"))
         assert(login.press_login_button(driver))
         assert(driver.current_url is not None and driver.current_url ==
                "http://localhost:54805/")
-
-    def test_add_friend_successful(self):
-        self.test_login_successful()
 
         driver.get("http://localhost:54805/Users")
 
@@ -37,12 +36,20 @@ class Test_Add_Friend():
     
     
     def test_add_friend_failed(self):
-        self.test_login_successful()
+        '''
+        A user is already friend with a user scenario
+        '''
+         # login
+
+        assert(login.enter_email(driver, "andreineagu672@gmail.com"))
+        assert(login.enter_password(driver, "Parola123!"))
+        assert(login.press_login_button(driver))
+        assert(driver.current_url is not None and driver.current_url ==
+               "http://localhost:54805/")
 
         driver.get("http://localhost:54805/Users")
 
-        assert(add_a_friend.enter_user_name(driver, "Lucia"))
+        assert(add_a_friend.enter_user_name(driver, "Marinela"))
         assert(add_a_friend.press_search_user_button(driver))
-        assert(add_a_friend.press_add_friend_button(driver))
-        assert(driver.current_url is not None and driver.current_url ==
-               "http://localhost:54805/Users")
+        assert(add_a_friend.press_add_friend_button(driver) == False)
+
